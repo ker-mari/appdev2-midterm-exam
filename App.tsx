617b7459174fb3c7d2437845834361bba5c7b9f9
ker-mari <magmayoker@gmail.com>
2026-05-10 18:18:13 +0800

    Using the navigation object

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import { createStaticNavigation } from '@react-navigation/native';
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Link } from '@react-navigation/native';
 import { Button } from '@react-navigation/elements';
 
+
 function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Link screen="Details">Go to Details</Link>
-      <Button screen="Details">Go to Details</Button>
+      <Button onPress={() => navigation.navigate('Details')}>
+        Go to Details
+      </Button>
     </View>
   );
 }
 
+// ... other code from the previous section
 
 function DetailsScreen() {
   return (
@@ -26,21 +33,11 @@ function DetailsScreen() {
 
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Home',
-  screenOptions: {
-    headerStyle: { backgroundColor: 'tomato' },
-  },
   screens: {
-    Home: {
-      screen: HomeScreen,
-      options: {
-        title: 'Overview',
-      },
-    },
+    Home: HomeScreen,
     Details: DetailsScreen,
   },
 });
-
-
 
 const Navigation = createStaticNavigation(RootStack);
 
